2de26fa25b99b135962f1d2241d9a985789b0ed6
aswinkrishnamohan <38403479+aswinkrishnamohan@users.noreply.github.com>
2021-05-03 13:06:35 +0530

    updates

--- SuperLeagueSprint1/src/main/java/com/s1/ms/sprint1/SuperLeagueSprint1.java
+++ SuperLeagueSprint1/src/main/java/com/s1/ms/sprint1/SuperLeagueSprint1.java
@@ -3,13 +3,18 @@ package com.s1.ms.sprint1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.s1.ms.sprint1.controller.InventoryController;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class SuperLeagueSprint1 {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(SuperLeagueSprint1.class, args);		
+//		SpringApplication.run(SuperLeagueSprint1.class, args);		
+		ConfigurableApplicationContext context = SpringApplication.run(SuperLeagueSprint1.class, args);
+		context.getBean(InventoryController.class).save();
 	}
 
 }
--- SuperLeagueSprint1/src/main/java/com/s1/ms/sprint1/controller/InventoryController.java
+++ SuperLeagueSprint1/src/main/java/com/s1/ms/sprint1/controller/InventoryController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -24,15 +25,16 @@ public class InventoryController {
 
 	private static final String fileLocation = "./inventory.xlsx";
 	
+	static Consumer<INVENTORY>  c1= p -> System.out.println(p);
+
 	@Autowired
 	InventoryService inventoryService;
-	
+
 	@GetMapping("/process")
 	public void triggerService() {
-		System.out.println("inside");
 		save();
 	}
-	
+
 	public List<INVENTORY> processFile() {
 		List<INVENTORY> inventoryList = new ArrayList<INVENTORY>();
 		try {
@@ -41,7 +43,6 @@ public class InventoryController {
 			Sheet sheet = workbook.getSheetAt(0);
 
 			Iterator<Row> itr = sheet.iterator(); // iterating over excel file
-			
 
 			int rowNumber = 0;
 			while (itr.hasNext()) {
@@ -71,11 +72,11 @@ public class InventoryController {
 						break;
 
 					case 2:
-						inventory.setPrice((Double)currentCell.getNumericCellValue());
+						inventory.setPrice((Double) currentCell.getNumericCellValue());
 						break;
 
 					case 3:
-						inventory.setQuantity((Double)currentCell.getNumericCellValue());
+						inventory.setQuantity((Double) currentCell.getNumericCellValue());
 						break;
 
 					default:
@@ -90,7 +91,6 @@ public class InventoryController {
 
 			workbook.close();
 
-			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -103,13 +103,27 @@ public class InventoryController {
 			List<INVENTORY> inventory = processFile();
 			inventoryService.saveInventory(inventory);
 			findAll();
+			summaryItem(inventory, 1);
+			SummaryPerDay(inventory);
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException("fail to store excel data: " + e.getMessage());
 		}
 	}
-	
+
 	public void findAll() {
 		System.out.println(inventoryService.findAllInventory());
+	}
+
+	private static void summaryItem(List<INVENTORY> inventoryList, int itemId) {
+		System.out.println("\nSummary of Single item with id:" + itemId);
+
+		inventoryList.stream().filter(p -> p.getId() == itemId).forEach(System.out::println);
+	}
+
+	private static void SummaryPerDay(List<INVENTORY> inventoryList) {
+
+		System.out.println("\nStock Summary per day:");
+		inventoryList.forEach(c1);
 	}
 }
